10c133f117945d690cab8c0e11cba0c5d3fb0e52
tomo-224 <98258121+tomo-224@users.noreply.github.com>
2024-03-08 16:57:34 +0900

    Update tpl.cpp

--- tpl.cpp
+++ tpl.cpp
@@ -345,7 +345,48 @@ namespace structure {
         return ((pre * rnd) - right * pows[r-l]) + left;
       }
   };
-
+  
+  struct str_hash {
+    size_t size;
+    mint4 rnd, rrnd;
+    string data;
+    vector<mint4> mp, rmp;
+    vector<mint4> prefix, suffix;
+    str_hash(size_t z = 0, mint4 r = mint4(0), string d = "") : size(z), rnd(r), data(d) {
+      if (z != 0) {
+        rrnd = mint4(1) / rnd;
+        mp = rmp = vector<mint4>(z, mint4(1));
+        for (size_t i = 1; i < z; i++) mp[i] = mp[i-1] * rnd;
+        for (size_t i = 1; i < z; i++) rmp[i] = rmp[i-1] * rrnd;
+        prefix = suffix = vector<mint4>(z, mint4(0));
+        prefix[0].set(data[0]);
+        for (size_t i = 1; i < z; i++) prefix[i] = prefix[i-1] * rnd + mint4(data[i]);
+        suffix[z-1].set(data.back());
+        for (int i = z-2; i >= 0; i--) suffix[i] = mint4(data[i]) * mp[z-i-1] + suffix[i + 1];
+      }
+    }
+    str_hash(size_t z, mint4 r, string d, vector<mint4>& mi, vector<mint4>& rmi) : size(z), rnd(r), data(d), mp(mi), rmp(rmi) {
+      rrnd = mint4(1) / rnd;
+      if (z != 0) {
+        prefix = suffix = vector<mint4>(z, mint4(0));
+        prefix[0].set(data[0]);
+        for (size_t i = 1; i < z; i++) prefix[i] = prefix[i-1] * rnd + mint4(data[i]);
+        suffix[z-1].set(data.back());
+        for (int i = z-2; i >= 0; i--) suffix[i] = mint4(data[i]) * mp[z-i-1] + suffix[i + 1];
+      }
+    }
+    mint4 hash(int l, int r){
+      if (l == r) return mint4(0);
+      assert(0 <= l && l < size);
+      assert(0 < r && r <= size);
+      assert(l < r);
+      return prefix[r-1] - (l == 0 ? mint4(0) : prefix[l-1]) * mp[r-l];
+    }
+    mint4 roll(int l, int r, int dist){
+      return hash(l+dist, r+dist);
+    }
+  };
+  
   void merge_arr(vector<ll>& ans, vector<ll>& a, vector<ll>& b) {
     ans = vector<ll>(0);
     size_t ai = 0, bi = 0;
