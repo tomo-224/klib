740690340fd66beec5fba135eb9839fb9845a7dc
tomo-224 <98258121+tomo-224@users.noreply.github.com>
2024-03-17 13:52:24 +0900

    Update bit.hpp

--- type/structure/bit.hpp
+++ type/structure/bit.hpp
@@ -2,52 +2,55 @@
 using ll = long long;
 using ull = unsigned long long;
 namespace structure {
-  class bit {
+  struct bit {
     ull value = 0;
-
+    
     //constructor
-    bit(int v = 0) : value(v) {};
+    bit(ull v = 0) : value(v) {};
 
     //operators
-    bit& operator+=(bit& other) { this.value += other.value; return this; }
-    bit& operator-=(bit& other) { this.value -= other.value; return this; }
-    bit& operator*=(bit& other) { this.value *= other.value; return this; }
-    bit& operator/=(bit& other) { this.value /= other.value; return this; }
-    bit& operator%=(bit& other) { this.value %= other.value; return this; }
-    bit& operator<<=(bit& other) { this.value <<= other.value; return this; }
-    bit& operator>>=(bit& other) { this.value >>= other.value; return this; }
-    bit& operator&=(bit& other) { this.value &= other.value; return this; }
-    bit& operator|=(bit& other) { this.value |= other.value; return this; }
-    bit& operator^=(bit& other) { this.value ^= other.value; return this; }
-    bit& operator++() { value++; return this; }
-    bit operator++(int) { bit tmp(value); value++; return tmp; }
-    bit& operator--() { value--; return this; }
-    bit operator--(int) { bit tmp(value); value--; return tmp; }
-    const bool operator[](const std::size_t t) { return (this.value & (1ull << t)); }
-    bit operator~() { return bit(~this.value); }
-    explicit operator bool() { return (this.value != 0); }
+    bit& operator+=(const bit& other) { value += other.value; return *this; }
+    bit& operator-=(const bit& other) { value -= other.value; return *this; }
+    bit& operator*=(const bit& other) { value *= other.value; return *this; }
+    bit& operator/=(const bit& other) { value /= other.value; return *this; }
+    bit& operator%=(const bit& other) { value %= other.value; return *this; }
+    bit& operator<<=(const bit& other) { value <<= other.value; return *this; }
+    bit& operator>>=(const bit& other) { value >>= other.value; return *this; }
+    bit& operator&=(const bit& other) { value &= other.value; return *this; }
+    bit& operator|=(const bit& other) { value |= other.value; return *this; }
+    bit& operator^=(const bit& other) { value ^= other.value; return *this; }
+    bit& operator++() { value++; return *this; }
+    bit operator++(signed) { bit tmp(value); value++; return tmp; }
+    bit& operator--() { value--; return *this; }
+    bit operator--(signed) { bit tmp(value); value--; return tmp; }
+    const bool operator[](const std::size_t t) { return (this->value & (1ull << t)); }
+    bit operator~() { return bit(~this->value); }
+    explicit operator bool() { return (this->value != 0); }
     bit operator!() { return !bool(*this); }
 
     //member function
     void set(std::size_t pos, ull n) {
-      this.value |= (1ull << pos);
-      this.value &= (n << pos);
+      value |= (1ull << pos);
+      value &= (n << pos);
       return;
     }
     bool at(std::size_t pos) {
-      return (this.value & (1 << pos));
+      return (value & (1 << pos));
     }
     bool next(const bit m) {
-      this++;
-      return (this < m);
+      value++;
+      return (value < m.value);
     }
-  }
+    ull& val(){
+      return value;
+    }
+  };
   
   //operators
   std::istream& operator>>(std::istream& is, bit& b){
-    return is >> b.val();
+    return is >> b.value;
   }
-  std::ostream& operator<<(std::ostream& os, bit& b){
+  std::ostream& operator<<(std::ostream& os, bit b){
     return os << b.val();
   }
   bit operator+(const bit& t1, const bit& t2) { return bit(t1) += t2; }
